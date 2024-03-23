13e531e40d4fa5e7f3854a9ad5218f600a182a6b
Bohdan Parydubets <bogdanparidubec@gmail.com>
2024-03-23 16:31:26 +0200

    Added logs, minor changes to db seed

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -9,7 +9,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     load_dotenv("../../.env.dev")
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_SQLALCHEMY_DATABASE_URI")
+    #SQLALCHEMY_DATABASE_URI = os.getenv("TEST_SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SECRET_KEY = os.getenv("SECRET_KEY")
     MEDIA_FOLDER = f"./project/static/"
 
@@ -18,5 +19,6 @@ class Config_Test(object):
     load_dotenv("../../.env.dev")
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("TEST_SQLALCHEMY_DATABASE_URI")
+    #SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SECRET_KEY = os.getenv("SECRET_KEY")
     MEDIA_FOLDER = f"./project/static/"
